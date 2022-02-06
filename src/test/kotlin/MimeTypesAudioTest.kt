82b89de676719bbfe937015bd8757880cce77685
Mark Whitaker <mark@mainwave.co.uk>
2022-02-06 13:06:54 +0000

    Add audio/* unit tests

--- src/test/kotlin/MimeTypesAudioTest.kt
+++ src/test/kotlin/MimeTypesAudioTest.kt
@@ -9,5 +9,53 @@ class MimeTypesAudioTest {
         Assert.assertEquals("audio/midi", MimeTypes.Audio.MIDI)
     }
 
-    // TODO: finish off tests
+    @Test
+    fun testAudioMp4() {
+        Assert.assertEquals("audio/mp4", MimeTypes.Audio.MP4)
+    }
+
+    @Test
+    fun testAudioMpeg() {
+        Assert.assertEquals("audio/mpeg", MimeTypes.Audio.MPEG)
+    }
+
+    @Test
+    fun testAudioOgg() {
+        Assert.assertEquals("audio/ogg", MimeTypes.Audio.OGG)
+    }
+
+    @Test
+    fun testAudioWav() {
+        Assert.assertEquals("audio/wav", MimeTypes.Audio.WAV)
+    }
+
+    @Test
+    fun testAudioWebm() {
+        Assert.assertEquals("audio/webm", MimeTypes.Audio.WEBM)
+    }
+
+    @Test
+    fun testAudioXAcc() {
+        Assert.assertEquals("audio/x-acc", MimeTypes.Audio.X_AAC)
+    }
+
+    @Test
+    fun testAudioXAiff() {
+        Assert.assertEquals("audio/x-aiff", MimeTypes.Audio.X_AIFF)
+    }
+
+    @Test
+    fun testAudioXMpegurl() {
+        Assert.assertEquals("audio/x-mpegurl", MimeTypes.Audio.X_MPEGURL)
+    }
+
+    @Test
+    fun testAudioXMsWma() {
+        Assert.assertEquals("audio/x-ms-wma", MimeTypes.Audio.X_MS_WMA)
+    }
+
+    @Test
+    fun testAudioXWav() {
+        Assert.assertEquals("audio/x-wav", MimeTypes.Audio.X_WAV)
+    }
 }
